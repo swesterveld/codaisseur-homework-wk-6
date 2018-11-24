90d407c202a24af1e5d56f91bd1e8706a0f1213f
Sil Westerveld <git@silwesterveld.com>
2018-11-24 22:45:19 +0100

    Implement POST /games endpoint that will create a new game based on name as the only input

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,6 +1,20 @@
-import {JsonController, Get} from 'routing-controllers'
+import {JsonController, Get, Post, BodyParam, HttpCode} from 'routing-controllers'
 
 import Game from './entity'
+
+enum Colors {
+  'red',
+  'blue',
+  'green',
+  'yellow',
+  'magenta'
+}
+
+const defaultBoard = [
+  ['o', 'o', 'o'],
+  ['o', 'o', 'o'],
+  ['o', 'o', 'o']
+]
 
 @JsonController()
 export default class GameController {
@@ -11,5 +25,20 @@ export default class GameController {
     return {
       games
     }
+  }
+
+  @Post('/games')
+  @HttpCode(201)
+  createGame(
+    @BodyParam('name') name: string
+  ): Promise<Game> {
+    const color = Colors[Math.trunc(Math.random()*Object.keys(Colors).length/2)]
+
+    const game = new Game()
+    game.name = name
+    game.color = color
+    game.board = JSON.parse(JSON.stringify(defaultBoard))
+
+    return game.save()
   }
 }
