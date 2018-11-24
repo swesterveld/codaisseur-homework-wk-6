53322c7aa0c6cc915d8fe91e266bc2e5001a615d
Sil Westerveld <git@silwesterveld.com>
2018-11-24 18:43:08 +0100

    Implement Game entity based on constraints given in step 1

--- src/games/entity.ts
+++ src/games/entity.ts
@@ -1,6 +1,18 @@
-import { Entity } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 import { BaseEntity } from 'typeorm/repository/BaseEntity'
 
 @Entity()
 export default class Game extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @Column('text', {nullable: false})
+  name: string
+
+  @Column('text', {nullable: false})
+  color: string
+
+  @Column('json', {nullable: false})
+  board: string
 }
