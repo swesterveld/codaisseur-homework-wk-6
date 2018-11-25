c1ff4cf9ee2543582cdbde85921793688bf58c59
Sil Westerveld <git@silwesterveld.com>
2018-11-25 07:32:58 +0100

    Remove obsolete validation experiment

--- src/games/entity.ts
+++ src/games/entity.ts
@@ -1,8 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 import { BaseEntity } from 'typeorm/repository/BaseEntity'
 import {Exclude} from 'class-transformer'
-import {Validate, Validator} from 'class-validator'
-import {ValidColorValidator} from "./validator"
+import {Validator} from 'class-validator'
 
 export enum Colors {
   'red',
@@ -24,9 +23,6 @@ export default class Game extends BaseEntity {
   })
   name: string
 
-  @Validate(ValidColorValidator, {
-    message: 'Color $value is invalid'
-  })
   @Column({
     type: 'text',
     nullable: false
