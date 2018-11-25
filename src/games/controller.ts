62f11914627e91537a2a5dd61d94504283d1b142
Sil Westerveld <git@silwesterveld.com>
2018-11-25 21:20:03 +0100

    (WIP) Refactor for less code with more structure for better overview

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -41,30 +41,21 @@ export default class GameController {
     @Body() update: Partial<Game>
   ) {
     const game = await Game.findOne(id)
-
     if (!game) {
       return new NotFoundError('Cannot find game')
     }
-    if (update.board) {
-      console.log(game)
-      /*
-      let old_board = JSON.parse(game.board)
-      let new_board = JSON.parse(update.board)
-      console.log('old_board', old_board)
-      console.log('new_board', new_board)
-      let moves = Game.moves(old_board, new_board)
-      console.log('n/o moves', moves)
-      */
-      if (Game.isValidMove(JSON.parse(game.board), JSON.parse(update.board))) {
-        throw new BadRequestError('Invalid move')
-      }
-      const merged = Game.merge(game, update)
 
-      if (Game.isValidColor(merged.color)) {
-        return merged.save()
-      } else {
-        throw new BadRequestError(`Invalid color`)
-      }
+    const validMove = Game.isValidMove(JSON.parse(game.board), JSON.parse(update.board))
+    if (!validMove) {
+      throw new BadRequestError('Invalid move')
     }
+
+    const validColor = Game.isValidColor(update.color)
+    if (!validColor) {
+      throw new BadRequestError(`Invalid color`)
+    }
+
+    const merged = Game.merge(game, update)
+    return merged.save()
   }
 }
